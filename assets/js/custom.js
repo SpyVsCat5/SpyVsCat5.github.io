4e25cecc53dadf3a1cc0319e891e7e4eda2d0630
Pavel Yermalovich <y.e.rmolovichp@gmail.com>
2017-06-16 12:14:15 -0400

    Fermé de la période estivale
    Fermé de la période estivale

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,11 @@
 function getSchedule() {
-    return ["Fermé"/*,
+    return ["Fermé",
             "10h - 18h",
             "10h - 20h",
             "13h - 18h",
             "10h - 19h",
             "10h - 19h30",
-            "13h - 21h"*/];
+            "13h - 21h"];
 }
 
 function getOpeningHours() {
